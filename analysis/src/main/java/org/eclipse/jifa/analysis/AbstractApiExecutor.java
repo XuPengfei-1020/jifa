ae6f090a498fcb1e1fca4c27a1b7ab6bf6ad06ed
xupengfei11 <xupengfei11@meituan.com>
2023-11-27 12:52:17 +0800

    Dispose snapshot synchronized to prevent from some problem caused by data inconsistency.

--- analysis/src/main/java/org/eclipse/jifa/analysis/AbstractApiExecutor.java
+++ analysis/src/main/java/org/eclipse/jifa/analysis/AbstractApiExecutor.java
@@ -331,11 +331,11 @@ public abstract class AbstractApiExecutor<Analyzer> implements ApiExecutor {
     }
 
     public void release(@ApiParameterMeta(targetPath = true) Path target) {
-        cachedAnalyzer.invalidate(target);
+        cleanAndDisposeAnalyzerCache(target);
     }
 
     public void clean(@ApiParameterMeta(targetPath = true) Path target) {
-        cachedAnalyzer.invalidate(target);
+        cleanAndDisposeAnalyzerCache(target);
         File errorLog = errorLogFile(target);
         if (errorLog.exists()) {
             if (!errorLog.delete()) {
@@ -354,5 +354,14 @@ public abstract class AbstractApiExecutor<Analyzer> implements ApiExecutor {
 
     protected final boolean isActive(Path target) {
         return cachedAnalyzer.getIfPresent(target) != null || buildingAnalyzer.containsKey(target);
+    }
+
+    private void cleanAndDisposeAnalyzerCache(Path target) {
+        // Dispose snapshot synchronized to prevent from some problem caused by data inconsistency.
+        Analyzer analyzer = cachedAnalyzer.getIfPresent(target);
+        cachedAnalyzer.invalidate(target);
+        if (analyzer != null) {
+            cachedAnalyzerRemoved(analyzer);
+        }
     }
 }
